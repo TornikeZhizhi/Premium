20bde75e2af379b60f48421cbabe49316ba4dbb1
tornikezhizhi <zhizhiashvilitornike@gmail.com>
2020-12-28 00:43:43 +0400

    add flat-html

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
 
 	$(".hidden_stare").mouseenter(function(){
-
+		console.log("s")
 		$(".hidden_stare").css("fill","rgba(0,0,0,.7)")
 	})
 
@@ -17,8 +17,12 @@ $(document).ready(function(){
 $("#Layer_1 .build_hover").each(function(index){
 
 	$(this).mouseenter(function(){
+		if (!$(this).hasClass("hidden_stare")) {
+
+
  		$('#Layer_2 .st'+(index + 1)+'').css("fill","rgba(255,255,255,.7)")
  		$('#Layer_2 .st'+(index + 1)+'').css("x","535")
+ 		}
 	
 	})
 
@@ -29,3 +33,10 @@ $("#Layer_1 .build_hover").each(function(index){
 	})
 
 })
+
+
+
+$('body').on('click','.link_path',function(){
+
+      window.location = $(this).data('url');
+    });
